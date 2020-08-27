f2194d656ffbb8c4fcb795dd423527f994816759
Ruben <rubenhooijer@aol.com>
2020-08-27 14:29:38 +0200

    Implemented all UI

--- ROCBrainGame/Assets/Scripts/UI/MemoryInputPanel.cs
+++ ROCBrainGame/Assets/Scripts/UI/MemoryInputPanel.cs
@@ -1,18 +1,25 @@
-﻿public class MemoryInputPanel : UIPanel
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MemoryInputPanel : UIPanel
 {
+    [Header("References: ")]
+    [SerializeField] private Image _conditionIcon;
+
     private ConditionButton currentConditionButton;
 
     public void Show(ConditionButton conditionButton)
     {
-        base.Open();
         currentConditionButton = conditionButton;
+        _conditionIcon.sprite = conditionButton.Condition.icon;
         Numpad.OnEnterNumber += currentConditionButton.SetNumber;
+        base.Open();
     }
 
     public override void Close()
     {
-        base.Close();
         Numpad.OnEnterNumber -= currentConditionButton.SetNumber;
+        base.Close();
     }
 
     private void OnDestroy()
